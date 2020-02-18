1b13366536419795717adeda1a098c2bb9c472e6
Andrew-1000 <andruambia95@gmail.com>
2020-02-18 06:45:32 +0000

    Create a DepartmentsTest to check whether it instantiate correctly,

--- src/test/java/DepartmentTest.java
+++ src/test/java/DepartmentTest.java
@@ -1,2 +1,31 @@
+import models.Departments;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DepartmentTest {
+    Departments departments = new Departments(1, "Finance", "Deals with money matters");
+
+    @Test
+
+    public void Departments_instantiatesCorrectly_true() {
+        assertEquals(true, departments instanceof Departments);
+    }
+
+    @Test
+
+    public void Departments_instantiatesWithDepartmentId_true() {
+        assertEquals(1, departments.getDepartment_id());
+    }
+
+    @Test
+    public void Departments_instantiatesWithDepartmentName_String(){
+        assertEquals("Finance", departments.getDepartment_name());
+    }
+
+    @Test
+    public void Department_instantiateswithDescription_String() {
+        assertEquals("Deals with money matters", departments.getDescription());
+    }
+
 }
