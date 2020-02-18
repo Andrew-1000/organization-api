b82d4d1a4155a1c9a22e0598ea974499e742cd2d
Andrew-1000 <andruambia95@gmail.com>
2020-02-18 06:28:04 +0000

    Create a test to check whether user initaites with a username to return a string

--- src/test/java/UsersTest.java
+++ src/test/java/UsersTest.java
@@ -4,10 +4,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class UsersTest {
-    Users testUsers = new Users();
+    Users testUsers = new Users ( 1, "Alex", 1, "Finance", "Accountant");
 
     @Test
     public void testUsers_instantiatesCorrectly_true() {
-        assertEquals(true, testUsers instanceof Users);
+        assertEquals(true, testUsers instanceof Users );
     }
+
+    @Test
+    public void Users_instantiatesWithUserId_int() {
+       assertEquals(1, testUsers.getUser_id());
+    }
+    @Test
+    public void Users_instantiatesWithUsername_String() {
+        assertEquals("Alex", testUsers.getUser_name());
+    }
+
 }
