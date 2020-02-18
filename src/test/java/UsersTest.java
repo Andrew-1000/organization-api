d577b9cb891e04a213ed4559cc019e55e45d14ec
Andrew-1000 <andruambia95@gmail.com>
2020-02-18 06:05:01 +0000

    Create test files UsersTest, DepartmentTesta and NewsTest in the test/java/ package

--- src/test/java/UsersTest.java
+++ src/test/java/UsersTest.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import models.Users;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class UsersTest {
+    Users testUsers = new Users();
+
+    @Test
+    public void users_instantiatesCorrectly_true(){
+        assertEquals(true, testUsers instanceof Users);
+    }
+    @Test
+    public void Users_instantiatesWithUserId_true() {
+        assertEquals(1, testUsers.getUser_id());
+    }
 }
