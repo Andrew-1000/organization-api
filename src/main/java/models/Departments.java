06afa97bcaae5c5516551c7a6e985da1cb6183cb
Andrew-1000 <andruambia95@gmail.com>
2020-02-20 00:12:50 +0300

    Department mode created to hold attributes of department, getter and setter methods && equal and hashCode methods, to be used for testing and actual implementaion of the API

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,55 @@
 package models;
 
+import java.util.Objects;
+
 public class Departments {
+
+    private int department_id;
+    private String department_name;
+    private String description;
+
+    public Departments(String department_name, String description) {
+
+        this.department_name = department_name;
+        this.description = description;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public String getDepartment_name() {
+        return department_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+
+    public void setDepartment_name(String department_name) {
+        this.department_name = department_name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof  Departments)) return false;
+        Departments that = (Departments) o;
+        return department_id == that.department_id &&
+                Objects.equals(department_name, that.department_name) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(department_id, department_name, description);
+    }
 }
