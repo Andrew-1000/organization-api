f2f9d4ab011182908a409c2df13df6e110614057
Andrew-1000 <andruambia95@gmail.com>
2020-02-19 23:49:20 +0300

    User instantiates correctly returns true - User initiates with user_name

--- src/test/java/UsersTest.java
+++ src/test/java/UsersTest.java
@@ -1,27 +1,23 @@
+import models.Departments;
 import models.Users;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class UsersTest {
-    Users testUsers = new Users ( 1, "Alex", 1, "Finance", "Accountant");
+    Departments departments = new Departments("Finance", "Deals with money matters");
+    Users testUsers = new Users ( "Alex", 1, "Finance", "Accountant");
 
     @Test
     public void testUsers_instantiatesCorrectly_true() {
         assertEquals(true, testUsers instanceof Users );
     }
-
-    @Test
-    public void Users_instantiatesWithUserId_int() {
-       assertEquals(1, testUsers.getUser_id());
-    }
+    
     @Test
     public void Users_instantiatesWithUsername_String() {
         assertEquals("Alex", testUsers.getUser_name());
     }
-    @Test
-    public void Users_instantiatesWithDepartmentName_String() {
-        assertEquals( "Finance", testUsers.getDepartment_name());
-    }
+
 
 }
