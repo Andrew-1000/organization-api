1132d9b92b2e62ee2835cbbe2d2e8a0cc34a8b9b
Andrew-1000 <andruambia95@gmail.com>
2020-02-20 00:51:17 +0300

    Declaration of methods that will be implemented in Data Access Objects(dao)

--- src/main/java/interfaces/DepartmentsDao.java
+++ src/main/java/interfaces/DepartmentsDao.java
@@ -1,4 +1,21 @@
 package interfaces;
 
-public class DepartmentDao {
+import models.Departments;
+import models.News;
+
+import java.util.List;
+
+public interface DepartmentsDao {
+
+    List<Departments> getAll();
+
+    Departments findById(int department_id);
+    void createDepartment(Departments departments);
+
+    //update
+    void update(int department_id, String department_name, String description);
+    void deleteById(int department_id);
+    void clearAll();
+
+    List<News> getAllNewsByDepartment(int department_id);
 }
--- src/main/java/interfaces/NewsDao.java
+++ src/main/java/interfaces/NewsDao.java
@@ -1,4 +1,24 @@
 package interfaces;
 
+import models.Departments;
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+
+    //Create
+    void add(News news);
+    void addNewsToDepartment(News news, Departments departments);
+
+
+    //Read
+    List<News> getAll();
+    List<Departments> getAllNewsForASpecificDepartment(int department_id);
+    News findById(int news_id);
+
+
+    //Delete
+    void deleteById(int news_id);
+    void clearAll();
 }
