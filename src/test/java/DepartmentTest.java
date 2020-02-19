a77852f9432f350daac73d1d3c453c078b81cdce
Andrew-1000 <andruambia95@gmail.com>
2020-02-19 23:38:58 +0300

    Create instances of Sql2oDepartmentDao Connection && Department - setup() method created to initialize the instanciated classes - shutdown() created to close connections - Tests for setting an Id to depatment created - Test for updating all department fields created

--- src/test/java/DepartmentTest.java
+++ src/test/java/DepartmentTest.java
@@ -1,21 +1,47 @@
+import dao.Sql2oDepartmentsDao;
 import models.Departments;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
 
 public class DepartmentTest {
-    Departments departments = new Departments(1, "Finance", "Deals with money matters");
 
+    private static Connection connection;
+    private static Sql2oDepartmentsDao depatmentDao;
+
+    Departments departments = new Departments("Finance", "Deals with money matters");
+    Departments testDepartment = setupDepartment();
+
+
+    @BeforeClass
+    public static void setUp() throws  Exception {
+        String connString = "jdbc:postgresql://localhost:5432/organization_api_test";
+        Sql2o sql2o = new Sql2o(connString, "postgres", "admin");
+        depatmentDao = new Sql2oDepartmentsDao(sql2o);
+        connection = sql2o.open();
+     }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("Clearing Database");
+        depatmentDao.clearAll();
+    }
+    @AfterClass
+    public static void shutDown() throws Exception {
+        connection.close();
+        System.out.println("Connection closed");
+        connection.setRollbackOnException(true);
+    }
     @Test
 
     public void Departments_instantiatesCorrectly_true() {
-        assertEquals(true, departments instanceof Departments);
-    }
-
-    @Test
-
-    public void Departments_instantiatesWithDepartmentId_true() {
-        assertEquals(1, departments.getDepartment_id());
+        assertTrue(departments instanceof Departments);
     }
 
     @Test
@@ -26,6 +52,39 @@ public class DepartmentTest {
     @Test
     public void Department_instantiateswithDescription_String() {
         assertEquals("Deals with money matters", departments.getDescription());
+    }
+    @Test
+    public void addingDepartmentSetsId() {
+        Departments departments = setupDepartment();
+        int id  = testDepartment.getDepartment_id();
+        depatmentDao.createDepartment(departments);
+        assertNotEquals(id, departments.getDepartment_id());
+    }
+
+    @Test
+    public void addedDepartmentReturnedThroughGetAllMethod(){
+        Departments testDepartment = setupDepartment();
+        assertEquals(2, depatmentDao.getAll().size());
+    }
+
+    @Test
+    public void noDepartmentReturnsEmptyList() {
+        assertEquals(1, depatmentDao.getAll().size());
+    }
+
+    private Departments setupDepartment() {
+        Departments departments = new Departments("Finance", "Deals with money matters");
+        depatmentDao.createDepartment(departments);
+        return departments;
+
+    }
+    @Test
+    public void updateCorrectlyUpdatesAllFields() throws Exception {
+        depatmentDao.update(testDepartment.getDepartment_id(), "Finance", "Deals with money matters");
+        Departments foundDepartment = depatmentDao.findById(testDepartment.getDepartment_id());
+        assertEquals("Finance", foundDepartment.getDepartment_name());
+        assertEquals("Deals with money matters", testDepartment.getDescription());
+
     }
 
 }
